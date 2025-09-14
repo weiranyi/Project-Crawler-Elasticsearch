484aef277d2cf1efde1dc7f33c0b9e4de829cc8c
亦蔚然 <88951231@qq.com>
2025-09-14 10:42:01 +0800

    fixBug: 修改实体类的有参构造标题、类容的接收顺序，以确保数据库标题存的是标题，内容字段存的是内容

--- src/main/java/com/github/weiranyi/entity/News.java
+++ src/main/java/com/github/weiranyi/entity/News.java
@@ -22,10 +22,10 @@ public class News {
 
     }
 
-    public News(String url, String content, String title) {
+    public News(String url, String title, String content) {
         this.url = url;
-        this.content = content;
         this.title = title;
+        this.content = content;
     }
 
     public News(News old) {
